c96d3e762df4b79f2e5be4c0e93f0143ebff13d6
shinwoo47 <84690786+shinwoo47@users.noreply.github.com>
2021-09-25 22:49:41 +0900

    권한shin

--- src/main/java/com/nextLevel/hero/mngRole/model/service/MngRoleService.java
+++ src/main/java/com/nextLevel/hero/mngRole/model/service/MngRoleService.java
@@ -1,5 +1,14 @@
 package com.nextLevel.hero.mngRole.model.service;
 
+import java.util.List;
+
+import com.nextLevel.hero.mngRole.model.dto.MngRankAuthDTO;
+import com.nextLevel.hero.mngRole.model.dto.MngRoleDTO;
+
 public interface MngRoleService {
+
+	List<MngRoleDTO> selectRank(int companyNo);
+
+	List<MngRankAuthDTO> selectRankAuth(int companyNo, String rank);
 
 }
--- src/main/java/com/nextLevel/hero/mngRole/model/service/MngRoleServiceImpl.java
+++ src/main/java/com/nextLevel/hero/mngRole/model/service/MngRoleServiceImpl.java
@@ -1,9 +1,13 @@
 package com.nextLevel.hero.mngRole.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nextLevel.hero.mngRole.model.dao.MngRoleMapper;
+import com.nextLevel.hero.mngRole.model.dto.MngRankAuthDTO;
+import com.nextLevel.hero.mngRole.model.dto.MngRoleDTO;
 
 @Service("mngRoleService")
 public class MngRoleServiceImpl implements MngRoleService {
@@ -14,5 +18,19 @@ public class MngRoleServiceImpl implements MngRoleService {
 	public MngRoleServiceImpl(MngRoleMapper mngRoleMapper) {
 		this.mngRoleMapper = mngRoleMapper;
 	}
+
+	@Override
+	public List<MngRoleDTO> selectRank(int companyNO) {
+		
+		return mngRoleMapper.selectRank(companyNO);
+	}
+
+	@Override
+	public List<MngRankAuthDTO> selectRankAuth(int companyNo, String rank) {
+
+		return mngRoleMapper.selectRankAuth(companyNo, rank);
+	}
+
+	
 	
 }
